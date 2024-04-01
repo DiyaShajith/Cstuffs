c5a9a001841c2cbdff8dbc30a3f0c65a8100092e
Diya Shajith K <diyashajith.2231@gmail.com>
2024-04-01 13:11:19 +0530

    01-04-2024

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,12 +4,20 @@ int main()
 	int n,rev=0,rem,temp;
 	printf("Enter the number : ");
 	scanf("%d",&n);
+	temp=n;
 	while(n>0)
 	{
 		rem=n%10;
 		rev=rev*10+rem;
 		n=n/10;	
 	}
-	printf("rev num is %d :",rev);
+	if(temp==rev)
+	{
+		printf("The num is palindrome \n");
+	}
+	else
+	{
+		printf("Not palindrome");
+	}
 	
 }
